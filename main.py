97f909b8833c43183aa30a87b777bd228a8de1f6
Ethel <89223773+alexaplaywoman@users.noreply.github.com>
2023-02-03 21:55:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -43,53 +43,74 @@ class shellLFS(cmd2.Cmd):
     def __init__(self):
         super().__init__()
         username = getpass.getuser() #user 
-        homedir = os.getcwd() #cwd
+        home_dir = os.getcwd() #cwd
         hostname = socket.gethostname() #hostname
       #  self.colors = mutilities.colors()
         
         self.default_to_shell = True #use default shell commands
-        self.prompt =f"{username}@{hostname}:{homedir}$"
+        self.prompt =f"{username}@{hostname}:{home_dir}$"
         self.maxrepeats = 3
         self.poutput("\nSHELL ORTIZ")
+
+    
+    def logregistroComandos():
+        logging.basicConfig(
+        format = '%(asctime)-5s %(name)-15s %(levelname)-8s %(message)s', 
+        level  = logging.INFO,      # Nivel de los eventos que se registran en el logger
+        filename = "logs_info.log", # Fichero en el que se escriben los logs
+        filemode = "a"              # a ("append"), en cada escritura, si el archivo de logs ya existe,
+                                    # se abre y añaden nuevas lineas.
+        )
+        if logging.getLogger('').hasHandlers():
+            logging.getLogger('').handlers.clear()
+
+    
+    def do_guardar():
+        import readline
+        readline.write_history_file('python_history.txt')
+        file = open('python_history.txt','r')
+        file.close()
 
 
 ###4.1. Copiar (no puede ser una llamada a sistema a la función cp) - copiar
     def do_copiar(self, phrase):
+        logging.info("copiar",phrase)
         space = " "
         new_arr = [i for i in phrase.split() if not space in i] #coloco cada palabra en un indice
-        dst_path1 = path.abspath(new_arr[-1])
-        open(new_arr[-1],'w')
-
-        for file in range(len(new_arr)-1) : #recorre los elementos 
-            open(new_arr[file],'a')
-             #el destino siempre va a ser el ultimo elemento
-            src_path = path.abspath(new_arr[file]) 
-            if bool(path.isdir(dst_path1)): 
-                dst_path = f"{dst_path1}/{new_arr[file]}"
-                shutil.copy(src_path, dst_path)
-            elif bool(path.isfile(dst_path1)): #si es un archivo se copia el archivo con otro nombre
-                shutil.copyfile(src_path ,dst_path1) 
+        dst_path1 = path.abspath(new_arr[-1])                   #el destino siempre va a ser el ultimo elemento
+        try:
+            for file in range(len(new_arr)-1) :                 #recorre los elementos 
+                if path.exists(new_arr[file]) == False:
+                    open(new_arr[file],'a')
+                src_path = path.abspath(new_arr[file]) 
+                if bool(path.isdir(dst_path1)): 
+                    dst_path = f"{dst_path1}/{new_arr[file]}"
+                    shutil.copy(src_path, dst_path)
+                if bool(path.isfile(dst_path1)):                 #si es un archivo se copia el archivo con otro nombre
+                    open(new_arr[-1],'w') 
+                    shutil.copyfile(src_path ,dst_path1) 
+            print("Exito en la copia")
+        except:
+            print("Error al copiar")
             
-        print("Exito en la copia")
+        
 
-###4.2. Mover - mover
+###4.2. Mover - mover      
     def do_mover(self,phrase):
         space = " "
-        new_arr = [i for i in phrase.split() if not space in i] #coloco cada palabra en un indice
-        dst_path1 = path.abspath(new_arr[-1]) #la direccion del destino siempre va a ser el ultimo elemento
-
-        for file in range(len(new_arr)-1) : #recorre los elementos 
-            src_path = path.abspath(new_arr[file]) #la direccion original
-
-            if bool(path.isdir(src_path)): #si es un directorio mueve cada archivo en el directorio ingresado
-                for file_name in os.listdir(src_path):
-                    source = f"{src_path}/{file_name}"
-                    shutil.move(source, dst_path1)
-
-            elif bool(path.isfile(src_path)): #si es un archivo, se mueve el archivo en el directorio ingresado   
-                dst_path = f"{dst_path1}/{new_arr[file]}"
-                shutil.move(src_path,dst_path)
-        print("Exito en mover",src_path)
+        new_arr = [i for i in phrase.split() if not space in i]  
+        dst_path = path.abspath(new_arr[-1])                        #destino de la carpeta
+        src_path = path.abspath(new_arr[0])                         #origen de la carpeta
+        if path.exists(dst_path) == False: 
+            raise FileNotFoundError(f"ERROR: {dst_path} no se encuentra")
+        try:
+            if path.samefile(src_path,dst_path) == False:           #si no son iguales 
+                shutil.move(src_path,dst_path)                      #se realiza mover
+                print("Exito en mover")
+        except:
+            print("Error. Ya existe en la carpeta")
+        else:
+            print("Error.No se pudo realizar la operacion mover")
 
 
 ###4.3. Renombrar - renombrar       
@@ -98,41 +119,96 @@ class shellLFS(cmd2.Cmd):
         new_arr = [i for i in phrase.split() if not space in i]
 
         old = path.abspath(new_arr[0])
-        old_name = Path(old).parent
-        new_name = new_arr[-1]
+        old_name = Path(old).parent                                     #adquirir en que directorio esta
+        new_name = new_arr[-1]                                          #adquirir el nombre nuevo
+        try:
+            if os.path.isfile(new_name) or os.path.isdir(new_name):
+                print("El archivo ya existe",new_name)
+            else:
+                # Renombrar el archivo
+                new1= f"{old_name}/{new_name}" 
+                new = path.abspath(new1)
+                os.rename(old, new)
+                print("Exito al renombrar")
+        except:
+            print(FAIL+"Error al renombrar")
 
-        if os.path.isfile(new_name):
-            print("The file already exists",new_name)
+
+###4.4. Listar un directorio (no puede ser una llamada a sistema a la función ls) - listar
+
+    def do_listar(self, argument):
+        if argument == "":
+            cwd = os.listdir()
+            print(cwd)
+        elif argument != "":
+            direct_arg = os.listdir(argument)
+            print(direct_arg)
         else:
-            # Rename the file
-            source = f"{old_name}/{new_name}"
-            print("este es el padre",old_name, "\n este es como se va a llamar",source)
-            #os.rename(old_name, new_name)
-            
-
-
+            print("Error al listar")
 
 
 ### 4.5. Crear un directorio - creardir
     def do_creardir(self, phrase):
         space = " "
-        if bool(re.search(space,phrase)):
+        try:
             for i in re.split(space,phrase):
                 os.mkdir(i)
+        except:
+            print("Error al crear directorio")
+
+
+###4.6. Cambiar de directorio (no puede ser una llamada a sistema a la función cd) - ir
+    def do_ir(self,phrase):
+        dst_dir = path.abspath(phrase)
+        if path.exists(dst_dir):
+            os.chdir(dst_dir)
         else:
-            os.mkdir(phrase)
+            print("No se realizo el cambio")
+
+
+###4.7. Cambiar los permisos sobre un archivo o un directorio - permisos
+    def do_permisos(argument):
+        new_arr = [i for i in argument.split() if not " " in i]
+        path = new_arr [-1]
+        perm = int(new_arr [0],8)   #convertimos en valor octal los permisos
+        try:
+            os.chmod(os.path.abspath(path), perm)
+        except Exception:
+            msg = "permisos: no se puedo realizar la operacion."
+            #logSystemError(msg)
+            print(msg)
+            return 1
+        return 0
 
 
 ### 4.11. Imprimir el directorio en el que se encuentra la shell actualmente - pwd
     def do_printdir(self,c):
         name = 'printdir'
         try:
-            cwd=os.getcwd()#obtener diretorio actual
+            cwd=os.getcwd()             #obtener diretorio actual
             #guardarParam = (name,cwd) 
-            self.poutput(cwd)#imprimir directorio actual
+            self.poutput(cwd)           #imprimir directorio actual
         except:
             print(FAIL+"Error printdir",cwd)
           #  self.logRegistroError(''.join(guardarParam))
+
+
+###4.13. Buscar un string en un archivo - grep
+    def do_sgrep(self,argument):
+        argnt = [i for i in argument.split() if not " " in i]
+        file_one = open(f"{argnt[-1]}.txt", "r")
+        if path.exists(f"{argnt[-1]}.txt"):
+            line_num = 0
+            flag = False
+            for line in file_one:
+                line_num += 1 
+                if re.search(argnt[0], line):
+                    print(f"Se encontro en la linea {line_num} {line}")
+                    flag =  True
+            if flag == False:
+                print("No se encontro en el archivo",argnt[0])
+        else:
+            print("Error no se encontro archivo",argnt[0])      
 
 if __name__ == '__main__':
     c = shellLFS()
